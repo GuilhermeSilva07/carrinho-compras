922bec7851ac7004fa3bc49f9ad88c44e5c57bad
Guilherme SIlva <gs2281172@gmail.com>
2024-04-06 17:10:12 -0300

    Adicionando itens no carrinho

--- js/app.js
+++ js/app.js
@@ -6,11 +6,17 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];                  //Quebrando o texto em partes
     let valorUnitario = produto.split('R$')[1]; 
-    let quantidade = document.getElementById('quantidade'); 
+    let quantidade = document.getElementById('quantidade').value; 
 
 
     //Calcular o preço, subtotal
-    let preco = quantidade.value * valorUnitario;  //Soma dos valores
+    let preco = quantidade * valorUnitario;  //Soma dos valores
+    let carrinho = document.getElementById('lista-produtos')
+
+    //Adicionando itens no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
 }
 
 
